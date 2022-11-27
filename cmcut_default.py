fafa450aa62c4994166700f82d04f6f18a67c653
Ryosuke Ohta <U.Onone@gmail.com>
2022-11-27 20:29:02 +0900

    Update cmcut_default.py.

--- cmcut_default.py
+++ cmcut_default.py
@@ -1,3 +1,4 @@
+import pathlib
 import sys
 
 from lib.cmcut import DurationSecUnits, FrameLoudness, ProgramScenes
@@ -18,6 +19,21 @@ if __name__ == '__main__':
         frame_per_sec, 
         duration_sec_units, 
         )
-    for section in program_scenes.scene_sections:
-        print (f"#{section[0], section[1] - section[0]}")
+    video_basename = pathlib.Path(wav_path).stem
+    total_duration = 0
+    for index, section in enumerate(program_scenes.scene_sections):
+        duration = section[1] - section[0]
+        print (
+            "docker run -v $(pwd):$(pwd) jrottenberg/ffmpeg -stats -i "
+            f"$(pwd)/{video_basename}.ts -c copy "
+            f"-ss {section[0]} -t {duration} " 
+            f"-map 0:v:0? -map 0:a:0? -map 0:a:1? $(pwd)/tmp_{video_basename}.ts"
+            )
+        print (
+            "docker run -v $(pwd):$(pwd) jrottenberg/ffmpeg -stats -i " 
+            f"$(pwd)/tmp_{video_basename}.ts -c:v libx264 -map 0:v:0? -map 0:a:0? -map 0:a:1? -f mp4 " 
+            f"$(pwd)/{video_basename}_{index}.m4v")
+        print (f"rm -f tmp_{video_basename}.ts")
+        total_duration += duration
+    print (f"#{total_duration}")
 
